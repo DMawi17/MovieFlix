0d85dccfc3a9ed9f8c51352070985805c35100a6
DMawi17 <asegda17@gmail.com>
2021-08-23 15:57:13 +0200

    third

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import "./style.css";
+// import Login from "./components/Login";
+import Home from "./components/Home";
+
 function App() {
-    return <div className="App">
-        <Home/>
-    </div>;
+    return (
+        <div className="App">
+            {/* <Login /> */}
+            <Home/>
+            {/* <Login /> */}
+            {/* <SignUp /> */}
+            {/* <MoviePage/> */}
+        </div>
+    );
 }
 
 export default App;
