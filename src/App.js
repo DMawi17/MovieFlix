2b950c791dbcc084b98252e82bcac8f50634675e
LeaCrazyBambi <lheynitz@gmail.com>
2021-08-24 16:21:49 +0200

    :)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import "./style.css";
 import Home from "./components/Home";
 import { useState } from "react";
+import Footer from "./components/Footer";
 
 function App({ data }) {
-    const [movie] = useState(data);
+  const [movie] = useState(data);
 
-    return (
-        <div className="App">
-            {movie.map((m) => (
-                <Home key={m.category} {...m} />
-            ))}
-        </div>
-    );
+  return (
+    <div className="App">
+      {movie.map((m) => (
+        <Home key={m.category} {...m} />
+      ))}
+
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/FooterStyles.js
@@ -0,0 +1,7 @@
+// import { Col, Container } from "react-bootstrap";
+
+// const FooterStyles = () => {
+//   return null;
+// };
+
+// export default FooterStyles;
