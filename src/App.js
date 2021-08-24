92101407a91dcfad38f95b0a6a5735c37927f450
Mxx1029 <lia.pim@icloud.com>
2021-08-24 17:15:57 +0200

    Map newMoviesImages array, add other images array sorted by categories

--- src/App.js
+++ src/App.js
@@ -1,22 +1,5 @@
 import "./style.css";
 import Home from "./components/Home";
-<<<<<<< HEAD
-import { useState } from "react";
-import Footer from "./components/Footer";
-
-function App({ data }) {
-  const [movie] = useState(data);
-
-  return (
-    <div className="App">
-      {movie.map((m) => (
-        <Home key={m.category} {...m} />
-      ))}
-
-      <Footer />
-    </div>
-  );
-=======
 
 function App() {
     return (
@@ -24,7 +7,6 @@ function App() {
             <Home />
         </div>
     );
->>>>>>> maxie
 }
 
 export default App;
