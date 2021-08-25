3eeafd62ad8c23e8fcdd4541dd4fd26d372e405e
DMawi17 <asegda17@gmail.com>
2021-08-25 16:11:45 +0200

    login text to link

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,71 +1,77 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useContext, useState } from "react";
 import UserContext from "../UserContext";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import { Form, Button, Nav } from "react-bootstrap";
+
 import "../style.css";
 
 var style = {
-  padding: "20px",
-  width: "50%",
+    padding: "20px",
+    width: "50%",
 };
 
 function Login() {
-  // const { user, setUser } = useContext(UserContext);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    // const { user, setUser } = useContext(UserContext);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-  function doLogin(e) {
-    e.preventDefault();
+    function doLogin(e) {
+        e.preventDefault();
 
-    // if (email !== "example@email.com") {
-    //   alert("Invalid email");
-    //   return;
-    // }
+        // if (email !== "example@email.com") {
+        //   alert("Invalid email");
+        //   return;
+        // }
 
-    // setUser({ id: 1, email, name: "Example User" });
-  }
+        // setUser({ id: 1, email, name: "Example User" });
+    }
 
-  return (
-    <Form style={style} className="loginuser mb-4 bg-dark" text={"light"}>
-      <Form.Text>C o n n e x i o n</Form.Text>
-      <Form.Group className="mb-4" controlId="formBasicEmail">
-        <Form.Label
-        // size="lg"
-        >
-          Connect to your account
-        </Form.Label>
-        <Form.Control
-          className="bg-light"
-          type="email"
-          placeholder="Enter email"
-        />
-      </Form.Group>
+    return (
+        <Form style={style} className="loginuser mb-4 bg-dark" text={"light"}>
+            <Form.Text>C o n n e x i o n</Form.Text>
+            <Form.Group className="mb-4" controlId="formBasicEmail">
+                <Form.Label
+                // size="lg"
+                >
+                    Connect to your account
+                </Form.Label>
+                <Form.Control
+                    className="bg-light"
+                    type="email"
+                    placeholder="Enter email"
+                />
+            </Form.Group>
 
-      <Form.Group className="mb-4" controlId="formBasicPassword">
-        <Form.Control
-          className="bg-light"
-          type="password"
-          placeholder="Password"
-        />
-      </Form.Group>
-      <Form.Group className="mb-4 bg-dark" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Remember me and stay connected" />
-      </Form.Group>
-      <Button variant="primary" type="submit" className="mb-4 bg-danger">
-        Connect
-      </Button>
-      <br />
-      <Button variant="primary" type="submit" className="mb-4 bg-danger ">
-        {/* Google Logo -- google */}
-        Connect with Google
-      </Button>
-      <Form.Group>
-        <Form.Text>Forgot your password?</Form.Text>
-        <Form.Text>Don't have an account? Register here.</Form.Text>
-      </Form.Group>
-    </Form>
-  );
+            <Form.Group className="mb-4" controlId="formBasicPassword">
+                <Form.Control
+                    className="bg-light"
+                    type="password"
+                    placeholder="Password"
+                />
+            </Form.Group>
+            <Form.Group className="mb-4 bg-dark" controlId="formBasicCheckbox">
+                <Form.Check
+                    type="checkbox"
+                    label="Remember me and stay connected"
+                />
+            </Form.Group>
+            <Button variant="primary" type="submit" className="mb-4 bg-danger">
+                Connect
+            </Button>
+            <br />
+            <Button variant="primary" type="submit" className="mb-4 bg-danger ">
+                {/* Google Logo -- google */}
+                Connect with Google
+            </Button>
+            <Form.Group>
+                <Nav.LInk>Forgot your password?</Nav.LInk>
+                <Nav.Link>Don't have an account? </Nav.Link>
+            </Form.Group>
+                <Nav.Link>Register here</Nav.Link>
+            <Nav.Item>
+            </Nav.Item>
+        </Form>
+    );
 }
 
 export default Login;
