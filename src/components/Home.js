ee134815fe8ac6d66e31fe39f0f16f0315099565
LeaCrazyBambi <lheynitz@gmail.com>
2021-08-25 15:14:48 +0200

    again

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,13 @@
 import Main from "./Main";
 import Header from "./Header";
 import Footer from "./Footer";
-import Login from "./Login";
 
 const Home = () => {
   return (
     <div>
-      <Login />
-      {/* <Header />
-            <Main />
-            <Footer /> */}
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 };
