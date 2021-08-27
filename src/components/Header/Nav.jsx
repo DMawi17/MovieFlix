94483478c61ab7c319c257a20ee9fd0ed18dbd98
DMawi17 <asegda17@gmail.com>
2021-08-27 18:33:04 +0200

    images removed, new nav bar add and styled

--- src/components/Header/Nav.jsx
+++ src/components/Header/Nav.jsx
@@ -1,10 +1,40 @@
+import { FaGhost } from "react-icons/fa";
+
 const Nav = () => {
-  return (
-    <div id="Nav">
-      <span>MovieFlix</span>
-      
-      
-    </div>)
+    return (
+        <div id="Nav">
+            <h1 id="Logo">
+                <a href="/">
+                    movie<span>/>lix</span>
+                </a>
+            </h1>
+            <ul id="Nav_Links">
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/">Films</a>
+                </li>
+                <li>
+                    <a href="/">Series</a>
+                </li>
+                <li>
+                    <a href="/">My List</a>
+                </li>
+            </ul>
+            <form>
+                <input
+                    type="text"
+                    name=""
+                    id="Search"
+                    placeholder="Search..."
+                />
+            </form>
+            <a id="Ghosty" href="/">
+                <FaGhost />
+            </a>
+        </div>
+    );
 };
 
 export default Nav;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,9 @@
-// import Main from "./Main";
-// import HeaderCard from "./Header/HeaderCard";
-// import HeaderNav from "./Header/HeaderNav";
-// import Footer from './Footer';
-// import Footer from "./Footer"
-// import MoviePage from "./MoviePage/MoviePage";
-
-import Main from "./MainPage/Main";
+import Nav from "./Header/Nav";
 
 const Home = () => {
     return (
         <div>
-            <Main />
-            {/* <HeaderNav /> */}
-            {/* <HeaderCard /> */}
-            {/* <Main /> */}
-            {/* <Footer /> */}
-            {/* <MoviePage /> */}
+            <Nav />
         </div>
     );
 };
