fbac361cb5cbfd27ad67289c0a9171b8e523a437
DMawi17 <asegda17@gmail.com>
2021-08-30 01:57:05 +0200

    last clean before search is fixed

--- README.md
+++ README.md
@@ -1,13 +1,3 @@
 # MovieFlix
 
--   we divide the page in three
-
-*   header
-*   main
-*   footer
-
-header => navBar, hero
-
-main => card container
-
-footer => socials..
+[Soon Come!](www.soon.com)
--- src/context/movie-hooks.js
+++ src/context/movie-hooks.js
@@ -6,13 +6,13 @@ export const useMovie = () => useContext(MovieContext);
 
 export const MovieProvider = ({ children }) => {
     const [IMG] = useState(IMG_URL);
-    // const [data, setData] = useState([]);
-    const [homeUrls, setHomeUrls] = useState(HomeUrls);
-    const [genreUrls, setGenreUrls] = useState(GenreUrls);
+    const [homeUrls] = useState(HomeUrls);
+    const [genreUrls] = useState(GenreUrls);
+    const [filmGenre] = useState([]);
     const [bannerPic, setBannerPic] = useState([]);
-    const [filmGenre, setFilmGenre] = useState([]);
-    const [search, setSearch] = useState([]);
     const [movies, setMovies] = useState([]);
+    const [search, setSearch] = useState([]);
+
 
     //.. Banner Effect
     useEffect(() => {
@@ -29,40 +29,24 @@ export const MovieProvider = ({ children }) => {
         }
         fetchBanner();
     }, [homeUrls.NetflixOriginals]);
-
+    
     const truncate = (str, maxLength = 150) => {
         return str?.length > maxLength ? str.slice(0, maxLength) + `…` : str;
     };
 
-    //.. Home Effect
+    //.. Search 
 
-    // const fetchUrl = genreUrls.map(url => url.fetchUrl)
-
-
-    // useEffect(() => {
-    //     async function fetchData() {
-    //         const req = await fetch(fetchUrl).then((res) => res.json());
-    //         setMovies(req.results);
-    //         return req;
-    //     }
-    //     fetchData();
-    // }, [fetchUrl]);
 
     return (
         <MovieContext.Provider
             value={{
                 IMG,
-                // data,
-                // setData,
                 homeUrls,
                 genreUrls,
                 bannerPic,
                 setBannerPic,
                 truncate,
-                setHomeUrls,
-                setGenreUrls,
                 filmGenre,
-                setFilmGenre,
                 search,
                 setSearch,
                 movies,
@@ -73,13 +57,3 @@ export const MovieProvider = ({ children }) => {
         </MovieContext.Provider>
     );
 };
-
-//..
-/* const test = "popular";
-
-const API_KEY = "api_key=22bd54fac3b17dbd99833a3dd26f1d90";
-const BASE_URL =
-    "https://api.themoviedb.org/3/search/movie?" + API_KEY + "&query=" + test;
-// const API_URL = BASE_URL + `/discover/movie?sort_by=${test}.desc&` + API_KEY;
-// const IMG_URL = "https://image.tmdb.org/t/p/w500/";
-const IMG_URL = "https://image.tmdb.org/t/p/w300_and_h450_bestv2"; */
