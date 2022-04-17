70fb54184df88255a4d4a989c193c8be96b9c557
DMawi17 <75326045+DMawi17@users.noreply.github.com>
2022-04-17 02:09:22 +0200

    MA-6 Complet the nav UI. Add extra nav items, search bar, new logo and login/registration button.

--- src/layout/nav/Nav.jsx
+++ src/layout/nav/Nav.jsx
@@ -1,44 +1,59 @@
 import { Link } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
+import { BsPersonFill } from "react-icons/bs";
+import { v4 } from "uuid";
 
 const NavBar = () => {
-    return (
-        <nav className="nav">
-            <Link to="/" className="logo">
-                Movie<span>Stream</span>
-            </Link>
+    const navElements = [
+        { path: "/", link: "Home" },
+        { path: "genre", link: "Genre" },
+        { path: "country", link: "Country" },
+        { path: "movies", link: "Movies" },
+        { path: "tvShows", link: "TV-Series" },
+        { path: "topImdb", link: "Top IMDB" },
+    ];
 
-            <ul className="nav_Links">
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="films">Genre</Link>
-                </li>
-                <li>
-                    <Link to="series">Country</Link>
-                </li>
-                <li>
-                    <Link to="series">Movies</Link>
-                </li>
-                <li>
-                    <Link to="series">TV-Series</Link>
-                </li>
-                <li>
-                    <Link to="series">TopIMDB</Link>
-                </li>
-                <div>
-                    <Link to="search" className="search_Btn">
-                        Search <FaSearch className="search_Icon" />
+    return (
+        <div className="container">
+            <nav className="nav">
+                <Link to="/" className="nav__logo">
+                    MovieSt.
+                </Link>
+
+                <ul className="nav__list">
+                    {navElements.map((el) => (
+                        <li key={v4()} className="nav__item">
+                            <Link
+                                to={`${el.path}`}
+                                className={`nav__link nav__${el.path}`}
+                            >
+                                {el.link}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+
+                <div to="search" className="nav__search ">
+                    <FaSearch className="nav__search-icon" />
+                    <label htmlFor="" className="nav__search-label">
+                        <input
+                            type="text"
+                            name="search"
+                            id="search"
+                            className="nav__search-input"
+                            placeholder="Enter your keywords..."
+                        />
+                    </label>
+                </div>
+
+                <div className="nav__login">
+                    <BsPersonFill className="nav__login-icon" />
+                    <Link className="nav__login-link" to="login">
+                        Login / Register
                     </Link>
                 </div>
-                <div>
-                    <li>
-                        <Link to="series">Login / Register</Link>
-                    </li>
-                </div>
-            </ul>
-        </nav>
+            </nav>
+        </div>
     );
 };
 
